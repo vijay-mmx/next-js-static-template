cd3621572db904a6f5b5dcaac4a5e4d38b6236a4
vijay-mmx <mvijay.mediamax@gmail.com>
2026-07-21 17:21:02 +0530

    changes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,21 +5,6 @@ import Quote from "@/components/home/quote";
 import Testimonials from "@/components/home/testimonials";
 import Verticals from "@/components/home/verticals";
 import Welcome from "@/components/home/welcome";
-import { Metadata } from "next";
-
-// CHANGE THIS Metadata according to you site
-export const metadata: Metadata = {
-    title: {
-        absolute: "meta title",
-    },
-    description: "meta descp",
-    openGraph: {
-        title: {
-            absolute: "meta title",
-        },
-        description: "meta descp",
-    }
-}
 
 export default function Home() {
     return (
